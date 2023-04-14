49d56bcfe2e68e9150c454485e14dd179da139ca
chagansingh <chagansingh9876@gmail.com>
2023-04-14 11:19:03 +0530

    http req for ip

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,7 +1,8 @@
 import { Fragment } from 'react';
-
+import AddressDisplay from '../http/http';
 import VerifyEmailForm from '../VerifyEmail/VerifyEmailForm';
 import classes from './StartingPageContent.module.css';
+import Exhibit from '../exhibit/exhibit';
 
 const StartingPageContent = () => {
 
@@ -9,6 +10,12 @@ const StartingPageContent = () => {
     <Fragment>
     <section className={classes.starting}>
       <h1>Welcome to Dashboard !!</h1>
+      <Exhibit name="Public IPv4 Address">
+        <AddressDisplay url='https://api.ipify.org?format=json' />
+      </Exhibit>
+      <Exhibit name="Public IPv6 Address">
+        <AddressDisplay url='https://api64.ipify.org?format=json' />
+      </Exhibit>
       <VerifyEmailForm/><br/>
     </section>
     </Fragment>
